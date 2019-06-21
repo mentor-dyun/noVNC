7c315e6882d2b76bd15d427be2f7c67670d35f9f
mentor-dyun <39041915+mentor-dyun@users.noreply.github.com>
2019-06-20 21:48:55 -0700

    Create runas_mentor.sh

---
+++ utils/runas_mentor.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# ----- change uid/gid of "mentor" user to values
+# ----- in $NEWUID and $NEWGID, or 500/500 if they
+# ----- are not set, then execute the arguments as
+# ----- a command.
+#
+# ---- usage:
+# ----    runas_mentor.sh ls -l .
+#
+: ${NEWUID=500}
+: ${NEWGID=500}
+#
+# ----- we can't change me user to a UID that is
+# ----- already in use
+#
+if [ $(grep -c ":\*:$NEWUID:" /etc/passwd) -gt 0 ]; then
+    echo "cannot change me uid to $NEWUID: uid is already in use by:"
+    echo $(grep -c ":\*:$NEWUID:")
+    exit 1
+fi
+usermod -u $NEWUID mentor
+#
+# ----- if a group with GID=$NEWGID already exists
+# -----    just switch me to that group, instead
+# -----    of changing the GID of the me group
+#
+if [ $(grep -c ":$NEWGID:" /etc/group) -eq 0 ]; then
+    groupmod -g $NEWGID mentor
+else
+    usermod -g $NEWGID mentor
+fi
+#
+# ---- change the ownership of all of the "me" files to match
+# ---- the new UID/GID
+# ----- and move the temp $HOME directory to /home/me
+# ----- because of the docker weirdness with permissions
+#
+chown -R mentor:$(id -g mentor)  /home/mentor
+usermod --home /home/mentor --move-home mentor
+#
+# ----- execute the command provides as the "mentor" user
+# ----- from the "mentor" home directory
+#
+cd /home/mentor
+exec sudo -E -u mentor "$@"
