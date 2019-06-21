b6e06cd13490d510adb27697ea1324c9e4265b49
mentor-dyun <39041915+mentor-dyun@users.noreply.github.com>
2019-06-20 21:40:13 -0700

    Create novnc_start.sh

---
+++ utils/novnc_start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# ----- start the X11 frame buffer (Xvfb)
+# ----- the openbox window manager
+# ----- and the x11vnc server
+#
+Xvfb ${DISPLAY} -screen 0 ${GEOMETRY}x${DEPTH} &
+sleep 5
+openbox-session &
+x11vnc -display ${DISPLAY} -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
+#
+# ----- start up the noVNC server so that browsers
+# ----- can connect on the exposed port (6080)
+#
+cd /home/mentor/noVNC
+./utils/launch.sh --vnc localhost:5900
